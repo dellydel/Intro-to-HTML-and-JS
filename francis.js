1592903f91e998a673f53410ed1f4fb8d45105e8
Francis Nwaobodo <somtocodestar@gmail.com>
2022-09-29 12:20:16 +0100

    JavaScript Assignment Completed

--- francis.js
+++ francis.js
@@ -1,15 +1,23 @@
 
 // Call two functions that ask for three parameters 'a', 'b' and 'c' and pass them in a math function
 
-function addMultiply(a, b, c) {
-    x1 = parseInt(a + b * c);
+function subtractMultiply() {
+    let a = window.prompt("First Number: ");
+    let b = window.prompt("Second Number: ");
+    let c = window.prompt("Third Number: ");
+
+    x1 = parseInt(a - b * c);
 
     console.log("The answer is equal to " + x1);
     alert(x1);
 }
 
-function addMultiplyInverse(a, b, c) {
-    x2 = parseInt(-a - b * -c);
+function subtractMultiplyInverse() {
+    let a = window.prompt("First Number: ");
+    let b = window.prompt("Second Number: ");
+    let c = window.prompt("Third Number: ");
+
+    x2 = parseInt(-a + b / -c);
 
     console.log("The answer is equal to " + x2);
     alert(x2);
