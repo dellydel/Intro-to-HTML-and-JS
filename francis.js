176b417ff3b330451476c7a9dd736be3a9b5227b
Francis Nwaobodo <somtocodestar@gmail.com>
2022-09-29 12:26:01 +0100

    JavaScript Assignment Completed

--- francis.js
+++ francis.js
@@ -6,19 +6,19 @@ function subtractMultiply() {
     let b = window.prompt("Second Number: ");
     let c = window.prompt("Third Number: ");
 
-    x1 = parseInt(a - b * c);
+    x1 = parseInt((a - b) * c);
 
     console.log("The answer is equal to " + x1);
-    alert(x1);
+    alert("The answer is equal to " + x1);
 }
 
-function subtractMultiplyInverse() {
+function powerDivide() {
     let a = window.prompt("First Number: ");
     let b = window.prompt("Second Number: ");
     let c = window.prompt("Third Number: ");
 
-    x2 = parseInt(-a + b / -c);
+    x2 = parseInt((a ** b) / -c);
 
     console.log("The answer is equal to " + x2);
-    alert(x2);
+    alert("The answer is equal to " + x2);
 }
