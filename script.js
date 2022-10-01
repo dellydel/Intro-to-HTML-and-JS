8c114953dfd53ffde490bc7f8901066a6686a335
Sanya Osimosu <sanya2ng@yahoo.com>
2022-10-01 10:18:59 -0500

    updated

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function multiplyFunction() {
+function arithmeticFunction(numOne, numTwo) {
+
+    // Math assignment
     let numOne = 60;
     let numTwo = 40;
 
     console.log(numOne * numTwo);
+    console.log(numOne - numTwo);
+    console.log(numOne / numTwo);
+    console.log(numOne % numTwo);
 }
         
