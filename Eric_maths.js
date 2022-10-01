020047f11dfe01b12ff96f7aa68bad817fda5fcc
El-eric5 <112766147+El-eric5@users.noreply.github.com>
2022-10-01 12:20:20 +0100

    added numbers

--- Eric_maths.js
+++ Eric_maths.js
@@ -1,13 +1,12 @@
- function MathFunction(){
+ function MathFunction(eric, agu){
     // set 2 variables
-    let 'eric'=20;
-    let 'agu'=2;
+    //let eric =20;
+    //let agu =2;
 
-    console.log(eric/agu)
+    console.log(eric/agu);
+    
 
-    let "mathfuction()"= 10;
-
-    console.log(MathFunction())
+    
 
 
 
