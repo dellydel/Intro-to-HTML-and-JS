a264f0bc6946fdd78d10ff32f6bf13ccaa005526
franklin obiajulu <franklinuniverse@gmail.com>
2022-10-12 15:00:58 +0100

    assignment correction

--- script.js
+++ script.js
@@ -1,29 +1,2 @@
-function displayMessage() {
-    let number1 = 500;
-    let number2 = 125;
-    console.log(number1 - number2) 
-
-
-let num1 = 3;
-let num2 = 3;
-console.log(num1 * num2)
- 
-//simple math to solve 
-
-let a = window.prompt("enter input for a");
-let b = window.prompt("enter input for b");
-let c = Math.pow(a,2) + Math.pow(b,2);
-console.log(c)
-
-let x = 300;
-let y = 10;
-let z = x / y;
-console.log(z) 
-
-let h = 28;
-let f = 5; 
-let g = h % f;
-console.log(g)
-}
 
 
