6167fe4eee33954885a9466f83c7070223a86dce
adeolu adeneye <henryarise1@gmail.com>
2022-10-17 23:39:00 -0500

    updated

---
+++ Adeolu_math.js
@@ -0,0 +1,14 @@
+function algebrasolution(){
+
+    let x = 20;
+    let y = 10;
+    let z = x * y;
+    let k = 40
+    let m = 80
+  
+  
+    console.log (x / y - z)
+    console.log (x * k -z)
+    console.log  (m /x*y)
+    
+    }
