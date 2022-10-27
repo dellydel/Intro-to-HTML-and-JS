1fd96ca561a77f057c50f0f6ac54f956c7cdec73
Sanya Osimosu <sanya2ng@yahoo.com>
2022-10-26 20:26:41 -0500

    newest

--- sanya_api.js
+++ sanya_api.js
@@ -2,5 +2,5 @@ function grabPreferrdActivity(){
 let url = ('https://jsonplaceholder.typicode.com/todos/1')
 fetch(url)
 .then(response => response.json())
-.then(json => document.getElementById("").innerHTML = 
+.then(json => document.getElementById("outputLabel").innerText = "Hello")
 }
