9c98ac559fcf8a53a6dcc6b58a6ab9f53b0e6c71
CharlyCee <charlznjoagwu@gmail.com>
2022-10-29 00:14:20 +0100

    Charles Updated API page

--- Charles_api.js
+++ Charles_api.js
@@ -1,9 +1,13 @@
-function myButton(){
-    let s = parseInt(document.getElementById("input_number").value);
-    fetch('https://jsonplaceholder.typicode.com/users/'+ s )
+function fechUsers(){
+    let number = parseInt(document.getElementById("input_number").value);
+    fetch('https://jsonplaceholder.typicode.com/users/'+ number )
     .then(response => response.json())
     .then(json =>document.getElementById("results").innerHTML = JSON.stringify(
-        "ID:" +json.id + "<br>Name:"+json.name + "<br>Email" + json.email 
+        "<b>ID:</b>" +json.id + "<br><b>Name:</b>"+json.name + "<br><b>Email:</b>" + json.email
+        + "<br><b><b>Street:</b></b>" +json.address.street  + "<br><b>Suite:</b>" +json.address.suite
+        + "<br><b>City:</b>" +json.address.city + "<br><b>Zipcode:</b>" +json.address.zipcode
+        + "<br><b>Phone:</b>" +json.phone  + "<br><b>Website:</b>" +json.website  
+
     ));
 
     
