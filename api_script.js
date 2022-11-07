6dbb6b72f2d26ea5fbbfb6ca57bb91b3ed11065f
Francis Nwaobodo <somtocodestar@gmail.com>
2022-11-07 15:56:08 +0100

    API Assignment Close To Completion

--- api_script.js
+++ api_script.js
@@ -1,6 +1,14 @@
 function siteInfo(){
-    const url = ('https://wordpress.org')
-    fetch(url)
+    let number = parseInt(document.getElementById('idNum').value);
+    fetch('https://jsonplaceholder.typicode.com/todos/' + number)
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => document.getElementById("output").innerHTML = JSON.stringify(
+        "<i>ID: </i>" + json.id +
+        "<br>" +
+        "<i>User ID: </i>" + json.userId +
+        "<br>" +
+        "<i>Title: </i>" + json.title +
+        "<br>" +
+        "<i>Completed: </i>" + json.completed
+    ));
 }
