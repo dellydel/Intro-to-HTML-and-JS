818e0bf9a1465f10e8cf74cd121fd6bb7bfd52b8
Francis Nwaobodo <somtocodestar@gmail.com>
2022-11-08 04:17:11 +0100

    Assignment

--- api_script.js
+++ api_script.js
@@ -1,14 +1,16 @@
 function siteInfo(){
     let number = parseInt(document.getElementById('idNum').value);
-    fetch('https://jsonplaceholder.typicode.com/todos/' + number)
+    fetch('https://jsonplaceholder.typicode.com/comments/' + number)
     .then(response => response.json())
     .then(json => document.getElementById("output").innerHTML = JSON.stringify(
+        "<i>Post ID: </i>" + json.postId +
+        "<br>" +
         "<i>ID: </i>" + json.id +
         "<br>" +
-        "<i>User ID: </i>" + json.userId +
+        "<i>Name: </i>" + json.name +
         "<br>" +
-        "<i>Title: </i>" + json.title +
+        "<i>Email: </i>" + json.email +
         "<br>" +
-        "<i>Completed: </i>" + json.completed
+        "<i>Comment: </i>" + json.body
     ));
 }
